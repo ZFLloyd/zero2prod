235079f543c51100b11c6ba9d9c0c85ff4903372
Florian Thelliez <florian.thelliez@gmail.com>
2021-08-22 17:50:00 +0200

    try fix CI again

--- scripts/wait_for_pg.sh
+++ scripts/wait_for_pg.sh
@@ -11,9 +11,6 @@ then
   export $(cat .env | sed 's/#.*//g' | xargs)
 fi
 
-until PGPASSWORD=$DATABASE_PASSWORD psql -h "$host" -U "postgres" -c '\q'; do
-  >&2 echo "Postgres is unavailable - sleeping"
-  sleep 1
-done
+
 
 >&2 echo "Postgres is up - executing command"
