4dde7b55907329e522f09b8554e8e225cd4dd42d
Florian Thelliez <florian.thelliez@gmail.com>
2021-08-22 17:28:28 +0200

    try something in CI

---
+++ scripts/wait_for_pg.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# wait-for-postgres.sh
+
+set -e
+
+host="$1"
+shift
+
+if [ -f .env ]
+then
+  export $(cat .env | sed 's/#.*//g' | xargs)
+fi
+
+until PGPASSWORD=$DATABASE_PASSWORD psql -h "$host" -U "postgres" -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Postgres is up - executing command"
